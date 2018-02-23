640cf33f4037b39adb29b90f247a1ac1941cab10
John Chritton <jchritton33@gmail.com>
2018-02-23 04:09:17 -0600

    Cleanup transfer

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -7,7 +7,8 @@ import { formValueSelector } from 'redux-form/immutable';
 
 import types from '../actions/constants';
 
-const getDisplayStatus = state => state.get('displayStatus');
+const getDisplayStatus = state =>
+  state.getIn(['streamReducer', 'displayStatus']);
 
 const getStreams = (state) => {
   const streams = state.getIn(['streamReducer', 'streams']);
@@ -17,64 +18,26 @@ const getStreams = (state) => {
 const getSearchValue = (state, props) =>
   formValueSelector('search')(state, 'searchInput');
 
-const createLoggingSelector = (mapFns, resultFn) => {
-  let _memoizedInputs;
-  let _memoizedResult;
-
-  function arraysEqual(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length !== b.length) return false;
-
-    // If you don't care about the order of the elements inside
-    // the array, you should sort both arrays here.
-
-    for (let i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  }
-
-  return (...params) => {
-    const inputs = mapFns.map(fn => fn(...params));
-
-    if (!arraysEqual(inputs, _memoizedInputs)) {
-      console.log('inputs are different');
-      _memoizedInputs = inputs;
-      _memoizedResult = resultFn(...inputs);
-    }
-
-    return _memoizedResult;
-  };
-};
-
 export const getLogins = (state, props) =>
   state.getIn(['streamReducer', 'twitchUsers']);
 
-export const getFilteredStreams = createLoggingSelector(
+export const getFilteredStreams = createSelector(
   [getStreams, getDisplayStatus],
   (streams, displayStatus) => {
     if (streams) {
-      console.log('in if statement');
       switch (displayStatus) {
         case types.SHOW_ALL:
-          console.log('in show all');
           return streams;
         case types.SHOW_ONLINE:
-          console.log('in online');
           return streams.filter(stream => stream.get('live') === true);
         case types.SHOW_OFFLINE:
-          console.log('in offline');
           return streams.filter(stream => stream.get('live') === false);
         default:
-          console.log('in default');
           return streams;
       }
     } else {
       return streams;
-      console.log('outside if');
     }
-    console.log('outside if-else');
   }
 );
 
